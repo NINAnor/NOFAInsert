5f5350729bdd29dfcfff66463807a137ac0f7980
Jakob Miksch <jakob.miksch@nina.no>
2017-05-10 12:31:18 +0200

    Reduce location types
    now only 'Norwegian VatnLnr', 'coordinates UTM32' and 'coordinates UTM33' are possible

--- nofa_insert.py
+++ nofa_insert.py
@@ -199,20 +199,10 @@ class NOFAInsert:
                         'Finish': 'FI'}'''
 
         self.locIDType_dict = {'Norwegian VatnLnr': 'no_vatn_lnr',
-                               'coordinates UTM32': 25832,
-                               'coordinates UTM33': 25833,
-                               }
+                          'coordinates UTM32': 25832,
+                          'coordinates UTM33': 25833,
+                          }
 
-        '''self.locIDType_dict = {'Norwegian VatnLnr': 'no_vatn_lnr',
-                              'Swedish SjoID': 'se_sjoid',
-                              'Finish nro': 'fi_nro',
-                              'coordinates UTM32': 25832,
-                              'coordinates UTM33': 25833,
-                              'coordinates UTM35': 25835,
-                              'coordinates lon/lat': 4326,
-                              'waterBody register name': '"waterBody"'
-                               }
-        '''
 
         self.ecotypes = {26164: ['Salmon', 'Landlocked Salmon', 'Relict Salmon'],
                          26165: ['Brown trout', 'Anadromous Brown Trout', 'Big Piscivorous Brown Trout', 'Fine-spotted Brown Trout', 'Danish River Trout'],
@@ -2362,6 +2352,7 @@ class NOFAInsert:
         self.dlg.dateEnd.setDate(self.today)
         self.dlg.verifiedDate.setDate(self.nextWeek)
 
+        """
         locIDType_dict = {'coordinates lon/lat': 4326,
                           'Norwegian VatnLnr': 'no_vatn_lnr',
                           'Swedish SjoID': 'se_sjoid',
@@ -2369,11 +2360,14 @@ class NOFAInsert:
                           'coordinates UTM32': 25832,
                           'coordinates UTM33': 25833,
                           'coordinates UTM35': 25835,
-                          'waterBody register name': '"waterBody"'}
+                          'waterBody register name': '"waterBody"'
+                          }
+        """
+
 
         # Add more location match options (e.g. coordinates)
 
-        locIDType_list = locIDType_dict.keys()
+        locIDType_list = self.locIDType_dict.keys()
         locIDType_list.sort()
         self.dlg.locIDType.addItems(locIDType_list)
         self.dlg.locIDType.setCurrentIndex(locIDType_list.index("Norwegian VatnLnr"))
