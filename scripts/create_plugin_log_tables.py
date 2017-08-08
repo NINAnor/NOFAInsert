04bee6646bdc933808a0b10957a78935f2c43fdd
svoboond <svoboond@gmail.com>
2017-08-08 09:04:49 +0200

    Add script for creating plugin log tables (see #56)

---
+++ scripts/create_plugin_log_tables.py
@@ -0,0 +1,143 @@
+import psycopg2
+
+
+# connection parameters to NODA DB official db
+
+pg_host=r'vm-srv-finstad.vm.ntnu.no'
+pg_user=r'<username>'
+pg_pwd='<password>'
+pg_db=r'nofa'
+pg_port=r'5432'
+
+try:
+    con = psycopg2.connect(
+        "host={} dbname={} user={} password= {} port={}"
+        .format(pg_host, pg_db, pg_user, pg_pwd, pg_port))
+    print u'connection successful'
+except:
+    print u'connection error'
+
+cur = con.cursor()
+
+
+# create plugin schema
+cur.execute(
+    """
+    CREATE SCHEMA    plugin;
+    """)
+
+con.commit()
+
+print u'schema "plugin"created'
+
+
+# create occurrence log table
+cur.execute(
+    """
+    CREATE TABLE    plugin.occurrence_log(
+                        occurrence_log_id serial NOT NULL,
+                        occurrence_id text NOT NULL,
+                        event_id text NOT NULL,
+                        dataset_id text NOT NULL,
+                        project_id text NOT NULL,
+                        reference_id text NOT NULL,
+                        location_id text NOT NULL,
+                        test boolean DEFAULT False,
+                        username text NOT NULL,
+                        insert_timestamp timestamp without time zone
+                            DEFAULT now(),
+                        update_timestamp timestamp without time zone
+                            DEFAULT now());
+    COMMENT ON TABLE    plugin.occurrence_log
+    IS                  'Log table storing info about all new occurrences inserted by NOFAInsert plugin.';
+    """)
+
+con.commit()
+
+print u'table "plugin.occurrence_log"created'
+
+
+# create dataset log table
+cur.execute(
+    """
+    CREATE TABLE    plugin.dataset_log(
+                        dataset_id integer NOT NULL,
+                        test boolean DEFAULT False,
+                        username text NOT NULL,
+                        insert_timestamp timestamp without time zone
+                            DEFAULT now(),
+                        update_timestamp timestamp without time zone
+                            DEFAULT now());
+    COMMENT ON TABLE    plugin.dataset_log
+    IS                  'Log table storing info about all new dataset metadata inserted by NOFAInsert plugin.';
+    """)
+
+con.commit()
+
+print u'table "plugin.dataset_log"created'
+
+
+# create project log table
+cur.execute(
+    """
+    CREATE TABLE    plugin.project_log(
+                        project_id integer NOT NULL,
+                        test boolean DEFAULT False,
+                        username text NOT NULL,
+                        insert_timestamp timestamp without time zone
+                            DEFAULT now(),
+                        update_timestamp timestamp without time zone
+                            DEFAULT now());
+    COMMENT ON TABLE    plugin.project_log \
+    IS                  'Log table storing info about all new project metadata inserted by NOFAInsert plugin.';
+    """)
+
+con.commit()
+
+print u'table "plugin.project_log"created'
+
+
+# create reference log table
+cur.execute(
+    """
+    CREATE TABLE    plugin.reference_log(
+                        reference_id integer NOT NULL,
+                        test boolean DEFAULT False,
+                        username text NOT NULL,
+                        insert_timestamp timestamp without time zone
+                            DEFAULT now(),
+                        update_timestamp timestamp without time zone
+                            DEFAULT now());
+    COMMENT ON TABLE    plugin.reference_log
+    IS                  'Log table storing info about all new reference metadata inserted by NOFAInsert plugin.';
+    """)
+
+con.commit()
+
+print u'table "plugin.reference_log"created'
+
+
+# create location log table
+cur.execute(
+    """
+    CREATE TABLE    plugin.location_log(
+                        location_id uuid NOT NULL,
+                        test boolean DEFAULT False,
+                        username text NOT NULL,
+                        location_name text DEFAULT NULL,
+                        insert_timestamp timestamp without time zone
+                            DEFAULT now(),
+                        update_timestamp timestamp without time zone
+                            DEFAULT now());
+    COMMENT ON TABLE    plugin.location_log
+    IS                  'Log table storing info about all new locations inserted by NOFAInsert plugin.';
+    """)
+
+con.commit()
+
+print u'table "plugin.location_log"created'
+
+
+con.close()
+
+print u'connection closed'
