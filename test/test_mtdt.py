3dba83a51e24d333e9f95af0deb19a2269538e2b
svoboond <svoboond@gmail.com>
2017-09-22 13:22:41 +0200

    Test: add metadata test

---
+++ test/test_mtdt.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ NOFAInsert
+                                 A QGIS plugin
+ Insert fish occurrence data to NOFA DB
+                              -------------------
+        begin                : 2017-01-09
+        git sha              : $Format:%H$
+        copyright            : (C) 2017 by NINA
+        contributors         : stefan.blumentrath@nina.no
+                               matteo.destefano@nina.no
+                               jakob.miksch@nina.no
+                               ondrej.svoboda@nina.no
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+import unittest
+import logging
+import ConfigParser
+
+LOGGER = logging.getLogger('QGIS')
+
+
+class TestMtdt(unittest.TestCase):
+    """Test that the plugin metadata contains mandatory information."""
+
+    def test_mtdt(self):
+        """Tests that the plugin metadata contains mandatory information."""
+
+        mand_mtdt = [
+            'name',
+            'description',
+            'version',
+            'qgisMinimumVersion',
+            'email',
+            'author']
+
+        mtdt_fp = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), os.pardir, 'metadata.txt'))
+        LOGGER.info(mtdt_fp)
+
+        parser = ConfigParser.ConfigParser()
+        parser.optionxform = str
+        parser.read(mtdt_fp)
+
+        sxn_name = 'general' 
+        msg = 'Cannot find section "{}" in "{}"'.format(sxn_name, mtdt_fp)
+
+        assert parser.has_section(sxn_name), msg
+
+        mtdt_items = parser.items(sxn_name)
+
+        for mtdt in mand_mtdt:
+            msg = (
+                'Cannot find metadata "{}" in section "{}" of "{}".'
+                .format(mtdt, sxn_name, mtdt_fp))
+
+            self.assertIn(mtdt, dict(mtdt_items), msg)
+
+if __name__ == '__main__':
+    unittest.main()
